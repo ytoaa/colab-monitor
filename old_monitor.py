0388d994c7842a68d56c08338c464e8a3d5f6796
ytoaa <naworldw@gmail.com>
2025-01-06 02:05:21 +0900

    script udpate

--- old_monitor.py
+++ old_monitor.py
@@ -1,19 +1,18 @@
 import os
-import pyzipper
+import zipfile
 import time
 import argparse
 
-def monitor_and_zip(source_folder, destination_folder, threshold=1, file_types=('.webp', '.png', '.jpeg'), password=None):
+def monitor_and_zip(source_folder, destination_folder, threshold, file_types=('.webp', '.png', '.jpeg'), compression_method=zipfile.ZIP_DEFLATED, compresslevel=9):
     """
     특정 폴더 및 하위 폴더를 모니터링하고, 지정된 파일 형식의 파일 개수가 threshold를 넘으면 파일들을 zip으로 압축하여 다른 폴더로 이동합니다.
-    비밀번호를 설정할 수 있습니다.
+    압축 생성 후 추가된 파일은 삭제하지 않습니다.
 
     Args:
         source_folder: 모니터링할 폴더의 경로.
         destination_folder: 압축된 zip 파일을 저장할 폴더 경로.
-        threshold: 파일 개수 임계값 (기본값: 1).
+        threshold: 파일 개수 임계값.
         file_types: 카운트할 파일 형식들의 튜플 (예: ('.webp', '.png', '.jpeg')).
-        password: 압축 파일에 설정할 비밀번호 (없으면 비밀번호 없음).
     """
     while True:
         files_to_zip = []  # zip에 포함될 파일 목록 저장
@@ -30,9 +29,7 @@ def monitor_and_zip(source_folder, destination_folder, threshold=1, file_types=(
             zip_filename = f"{source_folder_name}_{time.strftime('%Y%m%d%H%M%S')}.zip"
             zip_filepath = os.path.join(destination_folder, zip_filename)
 
-            with pyzipper.AESZipFile(zip_filepath, 'w', compression=pyzipper.ZIP_DEFLATED, encryption=pyzipper.WZ_AES) as zipf:
-                if password:
-                    zipf.setpassword(password.encode('utf-8'))
+            with zipfile.ZipFile(zip_filepath, 'w', compression=compression_method, compresslevel=compresslevel) as zipf:
                 for file_path in files_to_zip:
                     zipf.write(file_path, arcname=os.path.relpath(file_path, source_folder))
 
@@ -54,9 +51,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="모니터링하고 파일을 압축하는 스크립트입니다.")
     parser.add_argument("--source", required=True, help="모니터링할 폴더 경로")
     parser.add_argument("--destination", required=True, help="압축 파일 저장할 폴더 경로")
-    parser.add_argument("--threshold", type=int, default=1, help="파일 개수 임계값 (기본값: 1)")
+    parser.add_argument("--threshold", type=int, required=True, help="파일 개수 임계값")
     parser.add_argument("--file_types", nargs='*', default=['.webp', '.png', '.jpeg'], help="대상 파일 확장자 리스트")
-    parser.add_argument("--password", help="압축 파일에 설정할 비밀번호 (없으면 비밀번호 없음)")
     args = parser.parse_args()
 
     # 경로가 존재하지 않으면 생성
@@ -69,5 +65,5 @@ if __name__ == "__main__":
         destination_folder=args.destination,
         threshold=args.threshold,
         file_types=tuple(args.file_types),
-        password=args.password
+        compression_method=zipfile.ZIP_DEFLATED
     )
